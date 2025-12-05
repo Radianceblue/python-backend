54a8d463e78bd5df7bb591e5aeb37495a5fd539c
Radianceblue <radic.marina91@gmail.com>
2025-12-05 10:49:17 +0100

    Uppgift 3

--- src/app.py
+++ src/app.py
@@ -1,9 +1,37 @@
-# -*- coding: utf-8 -*-
+#Kör din FASTAPI-applikation: fastapi dev src/app.py
+#Om du vill återkomma till miljön senare och starta virtuella miljö: myenv\Scripts\Activate.ps1
+#Avaktivera miljön: deactivate
 
-from fastapi import FastAPI
+
+# -*- coding: utf-8 -*-
+#FastAPI is a modern, fast (high-performance), web framework for building APIs with Python based on standard Python type hints.
+from fastapi import FastAPI, Header
+
 
 app = FastAPI()
 
-@app.get("/")
-async def hello():
-    return {"Hello": "World!"}
+#path is / and operation is GET
+@app.get("/") 
+async def show_unicorn_list(): #this is our function that is below our decorator @app.get("/")
+    return {"message": "World!"}
+
+
+@app.get("/{id}")
+async def display_unicorn(id):
+   return {"id": id}
+
+@app.post("/")
+async def add_unicorn():
+    return {"new_unicorn": "new_unicorn"}
+
+@app.put("/{id}")
+async def update_unicorn(unicorn_name):
+    return {"unicorn_name": unicorn_name}
+
+@app.delete("/{id}")
+async def delete_unicorn(unicorn_id):
+    return {"unicorn_id": unicorn_id}
+
+
+
+
